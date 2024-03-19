1b37c35aeae059ec3d3858d96141e677983b866b
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2024-03-19 12:42:23 +0900

    Time: 0 ms (100%), Space: 7.6 MB (74.56%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector<double> res(2);
-        res[0] = celsius + 273.15;
-        res[1] = celsius * 1.8 + 32.0;
+        vector<double> res = {celsius + 273.15, celsius * 1.8 + 32.0};
         return res;
     }
 };
