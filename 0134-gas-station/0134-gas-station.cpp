e6ed2a4c23c191e28b1686f65bf9d52838b2a992
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2024-03-20 15:36:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -3,9 +3,17 @@ class Solution
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
     {
+        static int aa = 0;
+        if (i == 0)
+        {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            ++aa;
+        }
         int bal = 0, start = 0, total = 0;
         for(int i = 0, n = gas.size(); i< n; ++i)
         {
+            
             bal += gas[i] - cost[i];
             if(bal < 0)
             {
