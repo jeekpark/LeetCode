2667bad03442d58c19b03c7c2592979380757aa0
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2024-03-20 14:00:22 +0900

    Time: 0 ms (100%), Space: 7.1 MB (49.45%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int xorOperation(int n, int start) {
         int num = start;
         for (int i = 1; i < n; ++i)
-            num ^= start + 2 * i;
+            num ^= start + (2 * i);
         return num;
     }
 };
